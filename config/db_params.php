08399c53a0685a3096bc7eb134127a0fe7b9f558
denis <den.shabanov2015@yandex.ru>
2017-05-03 21:57:47 +0300

    kurs

---
+++ config/db_params.php
@@ -0,0 +1,9 @@
+<?php
+
+// Массив с параметрами подключения к базе данных
+return array(
+    'host' => 'localhost',
+    'dbname' => 'n92687o9_bd',
+    'user' => 'n92687o9_bd',
+    'password' => 'n92687o9_bd',
+);
